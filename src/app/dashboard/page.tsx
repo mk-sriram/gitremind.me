d7e6bafa74c15c44e6aff137cfcae4a854d6925a
Sriram MK <mksriram@umich.edu>
2024-09-14 15:05:38 -0400

    changed font

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { createClient } from "@/utils/supabase/client";
+
 const page = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4 bg-white w-[100%] ">
-        
-    </div>
+    <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4 bg-white w-[100%] "></div>
   );
 };
 
